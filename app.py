d88dc1ae3e6d0665601a4a20d3c287acc949174d
Seungjin Hwang <innovation100@naver.com>
2026-01-20 10:54:47 +0900

    제목 타이틀11

--- app.py
+++ app.py
@@ -252,7 +252,7 @@ st.markdown("""
     }
     
     .led-text::before {
-        content: '하루 1분 입력으로 매장의 모든 데이트를 분석하자!';
+        content: '하루 1분 입력이면, 모든 데이터 분석이 한방에!  •  두희야 현식아 잘 입력하자!  •  ';
         position: absolute;
         white-space: nowrap;
         color: #33ff33;
@@ -260,7 +260,7 @@ st.markdown("""
         font-size: 1.2rem;
         letter-spacing: 2px;
         text-shadow: none;
-        animation: ledBlink 1.5s ease-in-out infinite, ledScroll 15s linear infinite;
+        animation: ledBlink 1.5s ease-in-out infinite, ledScroll 20s linear infinite;
         line-height: 1.5rem;
         background: none !important;
         -webkit-background-clip: initial !important;
@@ -275,7 +275,7 @@ st.markdown("""
     
     @keyframes ledScroll {
         0% {
-            left: 100%;
+            left: 0;
         }
         100% {
             left: -100%;
